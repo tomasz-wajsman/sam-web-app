aecef498635d7f8539593245927d44470765680f
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-23 16:42:21 +0200

    Added routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Home from './Home';
+import Activities from './Activities';
+import ActivityDetails from './ActivityDetails';
+import ActivityAddForm from './ActivityAddForm';
+import ActivityEditForm from './ActivityEditForm';
 
 const AppRouter = () => {
   return (
     <Router history={{}}>
       <Switch>
         <Route exact path="/">
-          <p>Home</p>
+          <Home />
         </Route>
-        <Router exact path="/activities">
-          <p>Activities list</p>
-        </Router>
-        <Router exact path="/activities/show/:id">
-          <p>Single activity</p>
-        </Router>
+        <Route exact path="/activities">
+          <Activities />
+        </Route>
+        <Route exact path="/activities/details/:id">
+          <ActivityDetails />
+        </Route>
         <Route exact path="/activities/add">
-          <p>Add an activity</p>
+          <ActivityAddForm />
         </Route>
         <Route exact path="/activities/modify/:id">
-          <p>Modify an activity</p>
+          <ActivityEditForm />
         </Route>
         <Route>
           <p>404</p>
