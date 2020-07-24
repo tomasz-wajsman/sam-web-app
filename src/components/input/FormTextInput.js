f4671333dd4b5dbeb1bc8bb0f3b5bb2642850a7d
Tomasz Wajsman <tomasz.wajsman@outlook.com>
2020-07-24 11:58:30 +0200

    Added the label

--- src/components/input/FormTextInput.js
+++ src/components/input/FormTextInput.js
@@ -5,14 +5,18 @@ import PropTypes from 'prop-types';
 
 const FormTextInput = ({ value, placeholder, onChangeText, helperVisible, helperType, helperText, additionalHelperText }) => {
   return (
-    <TextField
-      style={{ padding: 5, height: 24 }}
-      value={value}
-      placeholder={placeholder}
-      error={helperText !== ''}
-      helperText={helperText}
-      onInput={e => onChangeText(e.target.value)}
-    />
+    <>
+      <TextField
+        style={{ padding: 5, height: 24 }}
+        value={value}
+        placeholder={placeholder}
+        label={placeholder}
+        error={helperText !== ''}
+        helperText={helperText}
+        onInput={e => onChangeText(e.target.value)}
+      />
+      <br />
+    </>
   );
 };
 
